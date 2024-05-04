dade589412b6f5518e63056727aea387c5862ea3
hemant <developer.trosnow@gmail.com>
2024-05-04 14:44:12 +0530

    Custom Hook

---
+++ 06-currency-converter/src/components/InputBox.jsx
@@ -0,0 +1,38 @@
+function InputBox({
+    label,
+
+    className = "",
+}) {
+
+
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+            <div className="w-1/2">
+                <label className="text-black/40 mb-2 inline-block">
+                    label
+                </label>
+                <input
+
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    type="number"
+                    placeholder="Amount"
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+
+                >
+
+                    <option value="usd">
+                        usd
+                    </option>
+
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
