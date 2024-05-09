928a8d3a4c5b8f7164d505ef7b527f15a40784c2
Hemantkumawat <hemantkumawat275@gmail.com>
2024-05-09 10:56:06 +0530

    removed un-necessary imports

--- 07-react-router/src/Layout.jsx
+++ 07-react-router/src/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 function Layout({ children }) {
     return (
