a762b2362df1f9c85973b2cb816ddc461099b5df
Hemantkumawat <hemantkumawat275@gmail.com>
2024-05-17 06:36:26 +0530

    TODO Context Created

--- 10-todo-context-local/src/context/TodoContext.js
+++ 10-todo-context-local/src/context/TodoContext.js
@@ -1,34 +1,45 @@
-import { createContext, useContext } from 'react';
-export const TodoContext = createContext({
-    todos: [
-        { id: 1, title: 'First Todo', completed: false },
-        { id: 2, title: 'Second Todo', completed: false },
-        { id: 3, title: 'Third Todo', completed: false },
-    ],
-    addTodo: (todo) => {
-        console.log('Add Todo Function')
+import React, { createContext, useContext, useState } from 'react';
 
-    },
-    toggleTodo: (id) => {
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                return {
-                    ...todo,
-                    completed: !todo.completed
-                };
-            }
-            return todo;
-        });
-        setTodos(updatedTodos);
-    },
-    deleteTodo: (id) => {
-
-    },
-    editTodo: (id, todo) => { },
-});
+const TodoContext = createContext();
 
 export const useTodo = () => {
     return useContext(TodoContext);
 }
 
-export const TodoProvider = TodoContext.Provider;
+export const TodoProvider = ({ children }) => {
+    const [todos, setTodos] = useState([
+        { id: 1, title: 'First Todo', completed: false },
+        { id: 2, title: 'Second Todo', completed: false },
+        { id: 3, title: 'Third Todo', completed: false },
+    ]);
+
+    const addTodo = (todo) => {
+        setTodos(prevTodos => [...prevTodos, todo]);
+    };
+
+    const toggleTodo = (id) => {
+        setTodos(prevTodos =>
+            prevTodos.map(todo =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+    };
+
+    const editTodo = (id, updatedTodo) => {
+        setTodos(prevTodos =>
+            prevTodos.map(todo =>
+                todo.id === id ? { ...todo, ...updatedTodo } : todo
+            )
+        );
+    };
+
+    return (
+        <TodoContext.Provider value={{ todos, addTodo, toggleTodo, deleteTodo, editTodo }}>
+            {children}
+        </TodoContext.Provider>
+    );
+};
