87b39836da4d89d1a22ade8404a37f95a1adbb63
Hemantkumawat <hemantkumawat275@gmail.com>
2024-07-11 22:00:25 +0530

    hmm

--- 12-mega-blog/src/appWrite/auth.js
+++ 12-mega-blog/src/appWrite/auth.js
@@ -3,12 +3,10 @@
 import { Client, Account, ID } from "appwrite";
 import config from "../config/app";
 export class AuthService {
-
     client = new Client();
     account;
     constructor() {
-        this.client.setEndpoint(config.appWrite.url)
-            .setProject(config.appWrite.projectId)
+        this.client.setEndpoint(config.appWrite.url).setProject(config.appWrite.projectId)
         this.account = new Account(this.client);
     }
     async createAccount({ email, password, name }) {
