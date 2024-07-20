bf76b4c23751a066c4cd471917adb6dc192b2ef0
Hemantkumawat <hemantkumawat275@gmail.com>
2024-07-20 23:09:06 +0530

    Navigation manu dynamic task done.

--- 12-mega-blog/src/components/Header/Header.jsx
+++ 12-mega-blog/src/components/Header/Header.jsx
@@ -1,19 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const authStatus = useSelector((state) => state.auth.status);
+    const navigate = useNavigate();
+    const navItems = [
+        {
+            name: 'Home',
+            slug: '/',
+            active: !authStatus
+        },
+        {
+            name: 'Login',
+            slug: '/',
+            active: !authStatus
+        },
+        {
+            name: 'Sign Up',
+            slug: '/',
+            active: !authStatus
+        },
+        {
+            name: 'All Posts',
+            slug: '/',
+            active: authStatus
+        },
+        {
+            name: 'Add Post',
+            slug: '/',
+            active: authStatus
+        }
+    ];
     return (
         <header className="bg-gray-800">
-            <div className="container mx-auto px-4 py-6 flex justify-between">
-                <h1 className="text-3xl text-white">My Website</h1>
+            <Container className="container mx-auto px-4 py-6 flex justify-between">
+                <div>
+                    <Link to='/'>
+                        <Logo width='70px' />
+                    </Link>
+                    <h1 className="text-3xl text-white">My Website</h1>
+                </div>
                 <nav className="">
                     <ul className="flex space-x-4">
-                        <li><a href="/" className="text-white hover:text-gray-300">Home</a></li>
-                        <li><a href="/about" className="text-white hover:text-gray-300">About</a></li>
-                        <li><a href="/contact" className="text-white hover:text-gray-300">Contact</a></li>
-                        <li><a href="/profile" className="text-white hover:text-gray-300">Profile</a></li>
+                        {navItems.forEach((item) =>
+                        (item.active ?
+                            (<li key={item.name}>
+                                <button href="/"
+                                    onClick={() => navigate(item.slug)}
+                                    className="text-white hover:text-gray-300">
+                                    {item.name}
+                                </button></li>)
+                            : ''))}
+                        {authStatus && (
+                            <li>
+                                <LogoutBtn />
+                            </li>
+                        )}
                     </ul>
                 </nav>
-            </div>
+            </Container>
         </header>
     );
 };
