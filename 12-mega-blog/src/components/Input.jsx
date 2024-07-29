12947329cb58c159f2e2da108dcb4ba3798b29b3
Hemantkumawat <hemantkumawat275@gmail.com>
2024-07-29 22:28:08 +0530

    border added

--- 12-mega-blog/src/components/Input.jsx
+++ 12-mega-blog/src/components/Input.jsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef(function Input({
     return (
         <div className="w-full">
             {label && <label className="inline-block mb-1 pl-1" htmlFor={id}>{label}</label>}
-            <input type={type} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50  ${className}`} />
+            <input type={type} className={`px-3 py-2 rounded-lg bg-white border border-1 rounded-lg text-black outline-none focus:bg-gray-50  ${className}`} />
         </div>
     )
 })
